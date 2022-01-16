18f8458ed66fa380f09d1a19fa5a425d20b5cddb
Gerardo Emmanuel Tovar Barragan <geemtovarba@ittepic.edu.mx>
2022-01-16 15:13:37 -0700

    fix

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/test", (_req, res) =>  {
+app.get("/", (_req, res) =>  {
   res.status(200).send("Hello world")
+})
+app.get("/test", (_req, res) =>  {
+  res.status(200).send("test 200")
 })
 module.exports = app;
